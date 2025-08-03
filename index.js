096a899c84e0aec38b4c623ba08d4d89942e5030
yashswi23 <yashswi2314@gmail.com>
2025-08-04 01:44:52 +0530

    Real-time Chat App

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import {createServer} from 'node:http'
+import { fileURLToPath } from 'node:url';
+import { dirname,join } from 'node:path';
+import { Server } from 'socket.io';
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+app.use(express.json());
+const dir_name = dirname(fileURLToPath(import.meta.url));
+app.get("/",(req,res)=>{
+    res.sendFile(join(dir_name,'index.html'));
+})
+// io.on('connection',(socket)=>{
+//     console.log('a user connected');
+//     socket.on('disconnect',()=>{
+//         console.log('user disconnected');
+//     })
+// });
+
+io.on('connection',(socket)=>{
+    console.log('a user connected');
+    socket.broadcast.emit('hi', 'A new user has Joined!');
+
+    socket.on('chat message',(msg)=>{
+        io.emit('chat message',msg);
+    })
+    socket.on('disconnect',()=>{
+        console.log('user disconnected');
+    })
+})
+
+server.listen(3000,()=>{
+    console.log(`Server is running on 3000`);
+})
